ef457b2bd780a41f2715fe792e35cb6581f87f7f
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-10-17 10:47:22 +0630

    resolved spring security deprecated methods issue

--- jwt-security/src/main/java/com/ninja/security/config/SecurityConfiguration.java
+++ jwt-security/src/main/java/com/ninja/security/config/SecurityConfiguration.java
@@ -22,6 +22,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -36,6 +37,20 @@ import lombok.AllArgsConstructor;
 @EnableMethodSecurity
 public class SecurityConfiguration {
 	
+	private static final String[] WHITE_LIST_URL = {
+			"/api/v1/auth/**",
+			"/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui/index.html"
+	};
+	
 	private final AuthenticationProvider authenticationProvider;
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 	private final LogoutHandler logoutHandler;
@@ -43,51 +58,31 @@ public class SecurityConfiguration {
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-			.authorizeHttpRequests()
-			.requestMatchers(
-					"/api/v1/auth/**",
-					"/v2/api-docs",
-	                "/v3/api-docs",
-	                "/v3/api-docs/**",
-	                "/swagger-resources",
-	                "/swagger-resources/**",
-	                "/configuration/ui",
-	                "/configuration/security",
-	                "/swagger-ui/**",
-	                "/webjars/**",
-	                "/swagger-ui/index.html"
-					).permitAll()
-			
-			.requestMatchers("/api/v1/demo-controller").hasAnyRole(USER.name(), ADMIN.name(), MANAGER.name())
-			
-			.requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
-			.requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
-			.requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
-			.requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
-			.requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
-			
-			/*
-			 * .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
-			.requestMatchers(GET, "/api/v1/admin/**").hasAuthority(ADMIN_READ.name())
-			.requestMatchers(POST, "/api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
-			.requestMatchers(PUT, "/api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
-			.requestMatchers(DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())
-			 */
-			
-			.anyRequest()
-			.authenticated()
-			.and()
-			.sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
+		http.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(req -> 
+				req.requestMatchers(WHITE_LIST_URL).permitAll()
+				
+					.requestMatchers("/api/v1/demo-controller").hasAnyRole(USER.name(), ADMIN.name(), MANAGER.name())
+					
+					.requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
+					.requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
+					.requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
+					.requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
+					.requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
+					.anyRequest()
+					.authenticated()				
+			)
+			.sessionManagement(session -> 
+				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			)
 			.authenticationProvider(authenticationProvider)
 			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-			.logout()
-			.logoutUrl("/api/v1/auth/logout")
-			.addLogoutHandler(logoutHandler)
-			.logoutSuccessHandler(logoutSuccessHandler); // clear SecurityContextHolder and return logoutResponse JSON 
-		
+			.logout(logout -> 
+				logout.logoutUrl("/api/v1/auth/logout")
+				.addLogoutHandler(logoutHandler)
+				.logoutSuccessHandler(logoutSuccessHandler) // clear SecurityContextHolder and return logoutResponse JSON
+			);
+			 
 		return http.build();
 	}
 
